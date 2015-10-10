913e281df6ce5b6ed82c1be08aa8ae9ced7f2774
Rainer <rainer@websimple.be>
2015-10-10 12:12:50 +0200

    import depuis fabrique

---
+++ lang/paquet-cidse_campaign_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cidse_campaign_description' => 'Site de campagne',
+	'cidse_campaign_nom' => 'Cidse Campaign',
+	'cidse_campaign_slogan' => 'Site de campagne',
+);
+
+?>
