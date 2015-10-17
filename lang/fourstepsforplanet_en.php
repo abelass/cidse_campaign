2fee065a33043ba79b9d70fb3b6a6ef8d22111b6
Rainer <rainer@websimple.be>
2015-10-17 12:29:45 +0200

    soaicla metatags

---
+++ lang/fourstepsforplanet_en.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION'))
+  return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+  // C
+  'fourstepsforplanet_titre' => 'Four Steps for the Planet',
+
+  // S
+  'share_video' => 'Four Steps for the Planet',
+  'share_video_meta' => 'Check out this Video on Four Steps for the Planet',
+  
+  // U
+  'upload_video' => 'Upload your video',
+);
+?>
