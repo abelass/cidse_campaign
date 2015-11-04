3fa0eac501eefd63c3a2addd05ddfe86e00a65e9
Rainer <rainer@websimple.be>
2015-11-04 17:28:46 +0100

    correct texte

--- lang/fourstepsforplanet_en.php
+++ lang/fourstepsforplanet_en.php
@@ -18,7 +18,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
   'share_video_description_twitter' => 'climateaction  at #COP21. Walk with me? #Change4Planet via @ChangeandCare',
   'share_video_description_og' => 'Click here to join this call to action by the campaign “Change for the planet – Care for the People” to #Change4Planet.',
   'share_video_title' => 'Thank you for walking with us!',
-  'share_video_title_meta' => 'Steps for the Planet | I just walked with hundreds of people to call for climate justice at Paris #COP21 and beyond. Walk with us? ',  
+  'share_video_title_meta' => '4 Steps for the Planet | I just walked with hundreds of people to call for climate justice at Paris #COP21 and beyond. Walk with us? ',  
   'share_video_meta' => 'Check out this Video on Four Steps for the Planet',
   
   // T
